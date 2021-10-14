7b5c4d3033ae8c39655b40ae92d9d18f68cd9f72
Stefan Müller <St.Mueller@hu-berlin.de>
2021-10-14 16:25:59 +0200

    added HeadLex2016

---
+++ 2016/config.pl
@@ -0,0 +1,15 @@
+$year       = "2016";
+$conf_date  = "July 25&ndash;29, $year";
+$proc_title = "Proceedings of the Joint 2016 Conference on Head-driven Phrase Structure Grammar and Lexical Functional Grammar";
+$location   = "Polish Academy of Sciences, Warsaw, Poland";
+$conf_name  = "HeadLex";
+$paper_prefix = "headlex2016";
+$editor     = "Arnold, Doug & Butt, Miriam & Crysmann, Berthold & King, Tracy Holloway & Müller, Stefan";
+$editor_g   = "Müller, Stefan";
+$crossref   = "BHKM${year}a-ed";
+$issn       = "1535-1793, 1098-6782";
+$maintainer = "<a href=\"http://hpsg.hu-berlin.de/~stefan/\">Stefan Müller</a>";
+
+# numbers of pages for the cover and editor's note.
+$page_offset = 3;
+
